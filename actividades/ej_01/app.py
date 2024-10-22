bac73c69db4038ece1a746eb81eb7dc1907cb762
Santino Ferrante <80297145+SantinoFerrante@users.noreply.github.com>
2024-10-22 15:09:05 -0300

    feat: entrego ej 1

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,22 @@
-# Aplicacion del servidor
+from microdot import Microdot
+from microdot import send_file
+
+# Crear una instancia de la aplicación Microdot
+app = Microdot()
+
+# Definir la ruta para la página principal ("/")
+@app.route('/')
+async def index(request):
+    # Enviar el archivo index.html cuando se accede a la ruta raíz
+    return send_file("index.html")
+
+# Definir una ruta dinámica para servir archivos estáticos
+@app.route('/<dir>/<file>')
+async def static(request, dir, file):
+    # Servir un archivo específico ubicado en el directorio dado
+    # `dir` es el nombre del directorio y `file` es el nombre del archivo
+    return send_file("/" + dir + "/" + file)
+
+# Iniciar la aplicación y escuchar las solicitudes
+app.run()
+
--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,24 @@
-# Configuracion inicial
+def do_connect():
+    # Importar el módulo de red para manejar conexiones Wi-Fi
+    import network
+    
+    # Crear una instancia del interface de estación (STA) de Wi-Fi
+    sta_if = network.WLAN(network.STA_IF)
+    
+    # Verificar si el dispositivo ya está conectado a la red
+    if not sta_if.isconnected():
+        print('connecting to network...')  # Imprimir un mensaje indicando que se está intentando conectar
+        sta_if.active(True)  # Activar el interface de estación (STA)
+        
+        # Intentar conectarse a la red Wi-Fi especificada
+        sta_if.connect("Cooperadora Alumnos", "")  # Aquí se especifica el nombre de la red y la contraseña (vacía en este caso)
+        
+        # Esperar hasta que se establezca la conexión
+        while not sta_if.isconnected():
+            print(".", end="25")  # Imprimir un punto cada vez que se verifica la conexión
+        
+    # Imprimir la configuración de la red una vez conectado
+    print('network config:', sta_if.ifconfig())
+    
+# Llamar a la función para iniciar el proceso de conexión
+do_connect()
