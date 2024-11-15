05595a3ac1705215525fbad685f3b72e819924ba
Santino Ferrante <80297145+SantinoFerrante@users.noreply.github.com>
2024-11-15 14:48:37 -0300

    feat: subo boot.py

--- actividades/ej_03/boot.py
+++ actividades/ej_03/boot.py
@@ -1,1 +1,15 @@
+def connect_to_wifi():
+    import network  # Se importa el módulo para manejo de red
+
+    wifi_interface = network.WLAN(network.STA_IF)  # Inicializa la interfaz Wi-Fi en modo cliente
+
+    if not wifi_interface.isconnected():
+        print('Conectando a la red Wi-Fi...')
+        wifi_interface.active(True)  # Activa la interfaz Wi-Fi
+        wifi_interface.connect('Cooperadora Alumnos', '')  # Intenta conectarse a la red especificada
+        while not wifi_interface.isconnected():
+            pass  # Espera hasta que se establezca la conexión
+    print('Configuración de red:', wifi_interface.ifconfig())  # Imprime los detalles de la conexión (IP, máscara, etc.)
+
+
 # Configuracion inicial
